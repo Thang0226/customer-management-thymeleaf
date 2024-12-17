bc0eda8168279870e50b84ed45c89d5a3dfd5c08
Thang0226 <thang.nd0226@gmail.com>
2024-12-17 14:12:08 +0700

    first commit

---
+++ src/main/java/com/codegym/controller/CustomerController.java
@@ -0,0 +1,13 @@
+package com.codegym.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customers")
+public class CustomerController {
+	@RequestMapping
+	public String list() {
+		return "index";
+	}
+}
