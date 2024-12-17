a88a389826a563d149dd9c4a95ec8b494a76f1b8
Thang0226 <thang.nd0226@gmail.com>
2024-12-17 17:31:57 +0700

    can delete & view customer information

--- src/main/java/com/codegym/controller/CustomerController.java
+++ src/main/java/com/codegym/controller/CustomerController.java
@@ -52,4 +52,23 @@ public class CustomerController {
 		redirectAttributes.addFlashAttribute("success", "Customer successfully updated");
 		return "redirect:/customers";
 	}
+
+	@GetMapping("/{id}/delete")
+	public String delete(@PathVariable int id, Model model) {
+		model.addAttribute("customer", customerService.findById(id));
+		return "/delete";
+	}
+
+	@PostMapping("/delete")
+	public String delete(Customer customer, RedirectAttributes redirect) {
+		customerService.remove(customer.getId());
+		redirect.addFlashAttribute("success", "Removed customer successfully");
+		return "redirect:/customers";
+	}
+
+	@GetMapping("/{id}/view")
+	public String view(@PathVariable int id, Model model) {
+		model.addAttribute("customer", customerService.findById(id));
+		return "/view";
+	}
 }
