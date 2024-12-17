96cd5337bd7da70d6faa908d000ae6c01249e77d
Thang0226 <thang.nd0226@gmail.com>
2024-12-17 14:46:46 +0700

    list information of all customers

--- src/main/java/com/codegym/controller/CustomerController.java
+++ src/main/java/com/codegym/controller/CustomerController.java
@@ -1,13 +1,24 @@
 package com.codegym.controller;
 
+import com.codegym.model.Customer;
+import com.codegym.service.CustomerService;
+import com.codegym.service.ICustomerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/customers")
 public class CustomerController {
-	@RequestMapping
-	public String list() {
+	private final ICustomerService customerService = new CustomerService();
+
+	@GetMapping
+	public String list(Model model) {
+		List<Customer> customerList = customerService.findAll();
+		model.addAttribute("customers", customerList);
 		return "index";
 	}
 }
